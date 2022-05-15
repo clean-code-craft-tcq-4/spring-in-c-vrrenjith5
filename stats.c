a8b5fdd06db968ff9d4b4fcd5648e67576c9e2ec
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-05-15 09:30:02 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -4,7 +4,21 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
     s.average = 0;
     s.min = 0;
-    s.max = 0;
+    s.max = 0;  
+    float sum = 0;
+    sum = s.min = s.max = numberset[0];
+    
+    for(int i =1; i<setlength; i++) {
+      if(s.min > numberset[i]) {
+		s.min = numberset[i];
+      }
+      if(s.max < numberset[i]) {
+		s.max = numberset[i];
+      }    
+      sum = sum + numberset[i];
+    }
+    s.average = sum/setlength;
+    return s;
 }
 
 int emailAlertCallCount = 0;
