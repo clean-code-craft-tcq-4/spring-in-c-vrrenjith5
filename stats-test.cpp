f9b01c76d7f13a11863db17982a4cd1fa4620090
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-05-15 11:32:23 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -1,10 +1,37 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "catch.hpp"
-#include "stats.h"
+//#include "stats.h"
 
 #include <stdlib.h>
 #include <math.h>
+
+struct Stats{
+	float average;
+	float min;
+	float max;
+};
+
+struct Stats compute_statistics(const float* numberset, int setlength) {
+    struct Stats s;
+    s.average = 0;
+    s.min = 0;
+    s.max = 0;
+    float sum = 0;
+    sum = s.min = s.max = numberset[0];
+
+    for(int i =1; i<setlength; i++) {
+      if(s.min > numberset[i]) {
+		s.min = numberset[i];
+      }
+      if(s.max < numberset[i]) {
+		s.max = numberset[i];
+      }
+      sum = sum + numberset[i];
+    }
+    s.average = sum/setlength;
+    return s;
+}
 
 TEST_CASE("reports average, minimum and maximum") {
     float numberset[] = {1.5, 8.9, 3.2, 4.5};
