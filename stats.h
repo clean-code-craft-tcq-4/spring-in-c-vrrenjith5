9d9bce3ac334aee7e58c1d3738e67f6a0788dc61
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-05-17 22:25:10 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -15,4 +15,7 @@ void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stat
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
 
+void emailAlerter();
+void ledAlerter();
+
 #endif
