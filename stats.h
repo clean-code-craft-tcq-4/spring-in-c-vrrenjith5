60791a9d3c25247a8dbfe680736dbec0d0b49aa2
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-05-17 23:24:05 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -2,9 +2,9 @@
 #define __STATS_H
 
 struct Stats{
-	float average;
-	float min;
-	float max;
+  float average;
+  float min;
+  float max;
 };
 
 Stats compute_statistics(const float* numberset, int setlength);
@@ -12,7 +12,6 @@ typedef void (*alerter_funcptr)();
 void check_and_alert(float maxthershold, alerter_funcptr alerters[], Stats computed);
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
-
 void emailAlerter();
 void ledAlerter();
 
